6aeadc5cf45249f7ff6e15f34ca32691bd53b3f3
github_DASorescu <srsdln97@gmail.com>
2022-06-29 16:59:01 +0200

    PALINDROMIA-COMPLETED

---
+++ js/palindromia.js
@@ -0,0 +1,27 @@
+// far inserire una parola all'utente
+// creare una funzione che ribalti la parola e la confronti con quella inserita dall'utente e restituisca come valore palindroma o non palindroma 
+// stampare in console il risultato 
+
+
+// //parola data dall'utente
+let userWord = prompt("inserisci una parola" , "anna");
+// messaggio che andro' a cambiare in base al risultato del check palindromia 
+let result;
+
+// ? FUNZIONE CHE MI STABILISCE SE LA PAROLA DA ME INSERITA E' PALINDROMA O MENO 
+function palindroma(){
+    let switchedWord = '';
+    for(let i = userWord.length -1 ; i >= 0; i--){
+        switchedWord += userWord[i];
+    }
+
+    console.log(switchedWord);
+
+    switchedWord === userWord ? result = ("e' palindroma") : result = ("non e' palindroma");
+
+    return result
+}
+
+// # STAMPO IN CONSOLE IL RISULTATO 
+console.log("la parola da te inserita " + palindroma());
+        
